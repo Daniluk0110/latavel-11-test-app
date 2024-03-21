1912860c69295428ec048b876c09045ebd29a970
Daniel S <sech-daniil@yandex.ru>
2024-03-21 12:20:20 +0300

    add beforeEach in login test

--- tests/Feature/Http/Controllers/LoginUserControllerTest.php
+++ tests/Feature/Http/Controllers/LoginUserControllerTest.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+
+beforeEach(function () {
+    Artisan::call('passport:install');
+});
+
 it('user can login', function () {
     $this->post('api/login')
         ->assertStatus(200);
